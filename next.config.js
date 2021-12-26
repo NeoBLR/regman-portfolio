d7a0b98bac4aa3767aa5b68c343d36dbd8495f32
regman <regxxxman@gmail.com>
2021-12-27 00:22:29 +0300

    pwa add

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
+const withPWA = require('next-pwa')
 const path = require('path')
 
-module.exports = {
-  reactStrictMode: true,
+const settings = {
+  env: {},
+  devIndicators: {
+    autoPrerender: false,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
 }
+
+// module.exports =
+//   process.env.NODE_ENV === 'development' ? settings : withPWA(settings)
+
+module.exports = withPWA(settings)
